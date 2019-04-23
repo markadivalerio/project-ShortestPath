17eef9bb07cf7fae7157491dfb7c86cb9ab34a39
Mark DiValerio ubuntu-laptop <markadivalerio@gmail.com>
2019-04-23 06:54:04 -0500

    fixing BF

--- impl/BellmanFord.java
+++ impl/BellmanFord.java
@@ -3,12 +3,9 @@ package impl;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.List;
 
-import objs.Edge;
-import objs.Graph;
-import objs.Route;
-import objs.ShortestPath;
-import objs.Vertex;
+import objs.*;
 
 public class BellmanFord extends ShortestPath
 {
@@ -36,36 +33,62 @@ public class BellmanFord extends ShortestPath
         }
         shortestDistTo.put(source, 0L);
         
-        Vertex src = graph.getVertex(source);
-        ArrayList<Vertex> originVertexes = graph.getConnectedVertices(src);
-        originVertexes.add(0, src);
+//        Vertex src = graph.getVertex(source);
+        ArrayList<String> originVertexes = new ArrayList<>(graph.getRealVertices());
+        originVertexes.remove(source);
+        originVertexes.add(0, source);
+//        ArrayList<Vertex> originVertexes = graph.getConnectedVertices(src);
+//        originVertexes.add(0, src);
         
-        for(Vertex start: originVertexes)
+        for(String startCode: originVertexes)
         {
+//        	System.out.println(startCode);
+        	Vertex start = graph.getVertex(startCode);
+        	
+//        	System.out.println(graph.getConnectedVertices(start));
 
-        	String startCode = start.getCode();
-
-        	for(Edge e: graph.getConnectedEdges(startCode))
+        	
+        	for(String eCode: graph.getEdges())
+//        	for(Vertex nextVertex: graph.getConnectedVertices(start))
+//        	for(Edge e: graph.getConnectedEdges(startCode))
         	{
+        		Edge e = graph.getEdgeFromECode(eCode);
+//        		Edge e = graph.findEdge(start, nextVertex);
+//        		if(e == null)
+//        			continue;
+//        		{
+//        			e = graph.findEdge(nextVertex, start);
+//        			if(e == null)
+//        				continue;
+//        		}
+        		Vertex startVertex = e.getOrigin();
         		Vertex nextVertex = e.getDestination();
-
+        		if(nextVertex.equals(start))
+        		{
+        			nextVertex = e.getOrigin();
+        			startVertex = e.getDestination();
+        		}
         		String nextVertexCode = nextVertex.getCode();
         		
-        		Long proposedVertexDistance = shortestDistTo.get(startCode) + e.getWeight();
+        		Long proposedVertexDistance = shortestDistTo.get(startVertex.getCode()) + e.getWeight();
+        		
+//        		System.out.println(startCode + " -> " + nextVertexCode + " = " + proposedVertexDistance);
 
 
         		if(proposedVertexDistance < shortestDistTo.get(nextVertexCode))
             	{
+//        			System.out.println(startCode + " -> " + nextVertexCode + " = " + proposedVertexDistance);
         			@SuppressWarnings("unused")
 					Long oldDist = shortestDistTo.put(nextVertexCode, proposedVertexDistance);
     				@SuppressWarnings("unused")
-					String oldStartStr = previous.put(nextVertexCode, startCode);
+					String oldStartStr = previous.put(nextVertexCode, startVertex.getCode());
 //        			if(Arrays.asList("PIT", "CDG", "JFK").contains(nextVertexCode) || Arrays.asList("HOU", "PIT", "JFK").contains(startCode))
 //        			{
 //        				System.out.println(oldStartStr + "->" + nextVertexCode + "("+oldDist+") changed to " + previous.get(nextVertexCode) + "->"+nextVertexCode + "("+proposedVertexDistance+")");
 //            		}
         		}
             }
+
         }
         
         if(shortestDistTo.get(destination) >= REALLY_BIG_NUMBER)
@@ -85,7 +108,7 @@ public class BellmanFord extends ShortestPath
         		throw new UnsupportedOperationException("Error - the graph contains a negative-weight cycle");
         	}
         }
-        
+//        System.out.println(shortestDistTo.keySet().size());
       //Shortest route has been found, but need to reconstruct route (reversed)
         System.out.println("Shortest path found, reconstructing path...");
 
